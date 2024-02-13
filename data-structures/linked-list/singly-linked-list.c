1d6aaa37357455f979c812739c5fa7f55b4c9f22
Muhammad <auwalmuhd365@gmail.com>
2024-02-13 14:47:39 +0100

    implement linked list and print its values

--- data-structures/linked-list/singly-linked-list.c
+++ data-structures/linked-list/singly-linked-list.c
@@ -1,8 +1,52 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+
+typedef struct node{
+    int item;
+    struct node *next;
+} node;
 
 int main(){
 
-    printf("Hello, World!");
+    node *head = NULL;
+    node *current = NULL;
+    
+    for (int i = 0; i < 5; i++){
+        node *newNode = (node *)malloc(sizeof(node));
 
+        if(newNode == NULL){
+            printf("Failed to allocate memory\n");
+            return 1;
+        }
+
+        newNode->item = i;
+        newNode->next = NULL;
+
+        if(head == NULL){
+            head = newNode;
+        } else {
+            current->next = newNode;
+        }
+
+        current = newNode;
+    }
+
+
+    printf("Printing Linked List: \n");
+    current = head;
+    while(current != NULL){
+        printf("%d\n", current->item);
+        current = current->next;
+    }
+
+    current = head;
+    while(current != NULL){
+        node *temp = current;
+        current = current->next;
+        free(temp);
+    }
+    
+    printf("Done and dusted\n");
     return 0;
 }
