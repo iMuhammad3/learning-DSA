b8796c1ec4472785e8bf35adf890e03340cf9687
Muhammad <auwalmuhd365@gmail.com>
2024-02-14 21:05:02 +0100

    fill list and insert items

--- data-structures/linked-list/doubly-linked-list.c
+++ data-structures/linked-list/doubly-linked-list.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Define the structure for each node in the doubly linked list
 typedef struct node
 {
     struct node *prev;
@@ -8,7 +9,104 @@ typedef struct node
     struct node *next;
 } node;
 
-int main ()
+node *HEAD = NULL;
+
+void fillList(int);
+void printList();
+void insertItem(int);
+void freeMemory();
+
+int main()
 {
-    
+    fillList(10);
+    insertItem(20);
+    printList();
+    freeMemory();
+}
+
+void fillList(int count)
+{
+
+    node *current = HEAD;
+
+    for (int i = 0; i < count; i++)
+    {
+        node *newNode = (node *)malloc(sizeof(node));
+
+        if (newNode == NULL)
+        {
+            printf("Failed to allocate memory\n");
+            exit(1);
+        }
+
+        newNode->prev = NULL;
+        newNode->item = i + 1;
+        newNode->next = NULL;
+
+        // If the list is empty, assign HEAD to the new node
+        if (HEAD == NULL)
+        {
+            HEAD = newNode;
+        }
+        else
+        {
+            // Go to the last node and append the new node to the list
+            while (current->next != NULL)
+            {
+                current = current->next;
+            }
+            current->next = newNode;
+            newNode->prev = current;
+        }
+
+        // Update current pointer to the newly added node
+        current = newNode;
+    }
+}
+
+void printList()
+{
+    node *temp = HEAD;
+    printf("Printing Linked List: \n");
+    while (temp != NULL)
+    {
+        printf("%d\n", temp->item);
+        temp = temp->next;
+    }
+}
+
+void insertItem(int item)
+{
+    node *newNode = malloc(sizeof(node));
+
+    newNode->prev = NULL;
+    newNode->item = item;
+    newNode->next = NULL;
+
+    if (HEAD == NULL)
+    {
+        HEAD = newNode;
+        return;
+    }
+
+    node *temp = HEAD;
+
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+    newNode->prev = temp;
+}
+
+void freeMemory()
+{
+    node *current = HEAD;
+    while (current != NULL)
+    {
+        node *temp = current;
+        current = current->next;
+        free(temp);
+    }
+    HEAD = NULL;
 }
