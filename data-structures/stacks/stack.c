fdf535bd5de58d4215112c9ea2f9426220b47fdd
Muhammad <auwalmuhd365@gmail.com>
2024-02-15 10:34:52 +0100

    declare all methods and implement isFull and isEmpty

--- data-structures/stacks/stack.c
+++ data-structures/stacks/stack.c
@@ -9,8 +9,39 @@ typedef struct
     int top;
 } Stack;
 
+bool isFull(Stack);
+bool isEmpty(Stack);
+void push(Stack, int);
+void pop(Stack);
+void display(Stack);
+
 int main()
 {
     Stack stack;
     stack.top = -1;
+}
+
+bool isFull(Stack stack)
+{
+    return stack.top == MAX_SIZE - 1;
+}
+
+bool isEmpty(Stack stack)
+{
+    return stack.top == -1;
+}
+
+void push(Stack stack, int item)
+{
+
+}
+
+void pop(Stack stack)
+{
+
+}
+
+void display(Stack stack)
+{
+    
 }
