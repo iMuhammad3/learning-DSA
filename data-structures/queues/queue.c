4eb67210425af198fa4431166197ed88abc33298
Muhammad <auwalmuhd365@gmail.com>
2024-02-16 07:53:04 +0100

    declare methods

---
+++ data-structures/queues/queue.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define MAX_SIZE 100
+
+typedef struct {
+    int array[MAX_SIZE];
+    int rear;
+    int front;
+} Queue;
+
+// method declarations
+void enqueue(Queue *, int);
+int dequeue(Queue *);
+void init(Queue *);
+void display(Queue *);
+
+int main(void)
+{
+    Queue queue;
+}
+
+void init(Queue *queue)
+{
+
+}
+
+void enqueue(Queue *queue, int value)
+{
+
+}
+
+int dequeue(Queue *queue)
+{
+
+}
+
+void display(Queue *queue)
+{
+
+}
