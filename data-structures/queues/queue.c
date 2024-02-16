0ade0b2b2f7b1ce42560b0f3b0ca8a760d3cb2db
Muhammad <auwalmuhd365@gmail.com>
2024-02-16 07:59:40 +0100

    declare and define isEmpty and isFull methods

--- data-structures/queues/queue.c
+++ data-structures/queues/queue.c
@@ -15,16 +15,33 @@ void enqueue(Queue *, int);
 int dequeue(Queue *);
 void init(Queue *);
 void display(Queue *);
+bool isEmpty(Queue *);
+bool isFull(Queue *);
 
 int main(void)
 {
     Queue queue;
+    init(&queue);
+    if(isEmpty(&queue))
+    {
+        printf("Empty\n");
+    }
 }
 
 void init(Queue *queue)
 {
     queue->front = -1;
     queue->rear = -1;
+}
+
+bool isEmpty(Queue *queue)
+{
+    return queue->rear == queue->front;
+}
+
+bool isFull(Queue *queue)
+{
+    return queue->rear == MAX_SIZE - 1;
 }
 
 void enqueue(Queue *queue, int value)
@@ -34,10 +51,10 @@ void enqueue(Queue *queue, int value)
 
 int dequeue(Queue *queue)
 {
-
+    return 1;
 }
 
 void display(Queue *queue)
 {
-
+    
 }
