610b0a4b83e8383bc7961c6cef9250962ca39fb1
Muhammad <auwalmuhd365@gmail.com>
2024-02-16 14:53:25 +0100

    implement all methods

--- data-structures/queues/circular-queue.c
+++ data-structures/queues/circular-queue.c
@@ -11,14 +11,79 @@ typedef struct {
 } Queue;
 
 // method declarations
-void enqueue(Queue *, int);
-int dequeue(Queue *);
 void init(Queue *);
-void display(Queue *);
 bool isEmpty(Queue *);
 bool isFull(Queue *);
+void enqueue(Queue *, int);
+int dequeue(Queue *);
+void display(Queue *);
 
 int main(void)
 {
 
+}
+
+void init(Queue *queue)
+{
+    queue->front = -1;
+    queue->rear = -1;
+}
+
+bool isEmpty(Queue *queue)
+{
+    return queue->front == -1;
+}
+
+bool isFull(Queue *queue)
+{
+    return (queue->rear + 1) % MAX_SIZE == queue->front;
+}
+
+void enqueue(Queue *queue, int item)
+{
+    if(isFull(queue))
+    {
+        printf("Queue Overflow!\n");
+        return;
+    }
+    if(isEmpty(queue))
+    {
+        queue->front = 0;
+    }
+    queue->rear = (queue->rear + 1) % MAX_SIZE;
+    queue->array[queue->rear] = item;
+}
+
+int dequeue(Queue *queue)
+{
+    if(isEmpty(queue))
+    {
+        printf("Queue Underflow!\n");
+        exit(1);
+    }
+    int value = queue->array[queue->front];
+    if(queue->front == queue->rear)
+    {
+        queue->front = -1;
+        queue->rear = -1;
+    }
+    else 
+    {
+        queue->front = (queue->front + 1) % MAX_SIZE;
+    }
+    return value;
+}
+
+void display(Queue *queue)
+{
+    if(isEmpty(queue))
+    {
+        printf("Empty Queue\n");
+        return;
+    }
+    printf("Items in Queue: ");
+    for (int i = queue->front+1;i <= queue->rear;i++)
+    {
+        printf("%d\n", queue->array[i]);
+    }
 }
