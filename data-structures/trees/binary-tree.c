33e7e18d3074c2af52ef9a691b2acee1634567e5
Muhammad <auwalmuhd365@gmail.com>
2024-02-19 16:56:27 +0100

    finish binary tree implementation

--- data-structures/trees/binary-tree.c
+++ data-structures/trees/binary-tree.c
@@ -8,13 +8,24 @@ typedef struct TreeNode
     struct TreeNode *left, *right;
 } TreeNode;
 
-//method declarations
+// method declarations
 TreeNode *createNode(int);
 TreeNode *insertNode(TreeNode *, int);
 TreeNode *searchNode(TreeNode *, int);
+void displayTree(TreeNode *);
+void deallocate(TreeNode *);
 
 int main()
 {
+    TreeNode *root = createNode(100);
+    for (int i = 1; i < 10; i++)
+    {
+        insertNode(root, i * 10);
+    }
+    displayTree(root);
+    printf("\n");
+
+    deallocate(root);
 }
 
 TreeNode *createNode(int data)
@@ -29,19 +40,52 @@ TreeNode *createNode(int data)
 TreeNode *insertNode(TreeNode *root, int data)
 {
     // if tree is empty
-    if(root == NULL) return createNode(data);
-    if(data <= root->data)
+    if (root == NULL)
+        return createNode(data);
+    if (data <= root->data)
     {
         root->left = insertNode(root->left, data);
     }
-    else 
+    else
     {
         root->right = insertNode(root->right, data);
     }
     return root;
 }
 
-TreeNode *searchNode(TreeNode *root, int data)
+TreeNode *searchNode(TreeNode *root, int key)
 {
+    if (root == NULL)
+    {
+        printf("Empty Tree\n");
+        return NULL;
+    }
 
+    if (root->data == key)
+        return root;
+
+    if (key < root->data)
+        return searchNode(root->left, key);
+
+    return searchNode(root->right, key);
+}
+
+void displayTree(TreeNode *root)
+{
+    if (root != NULL)
+    {
+        displayTree(root->left);
+        printf("%d ", root->data);
+        displayTree(root->right);
+    }
+}
+
+void deallocate(TreeNode *root)
+{
+    if (root != NULL)
+    {
+        deallocate(root->left);
+        deallocate(root->right);
+        free(root);
+    }
 }
