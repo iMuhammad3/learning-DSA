bd9281581abc062dd756f4727036a6ee3bd0d63b
Muhammad <auwalmuhd365@gmail.com>
2024-02-25 10:26:02 +0100

    fix error
    array was not sorted

--- algorithms/searching/BinarySearch.java
+++ algorithms/searching/BinarySearch.java
@@ -3,7 +3,7 @@ package algorithms.searching;
 public class BinarySearch {
 
     public static void main(String[] args) {
-        int array[] = { 1, 2, 40, 5, 6, 700 };
+        int array[] = { 1, 2, 4, 5, 6, 700 };
         int value = 5;
         int res = binarySearch(array, value);
         if (res == -1) {
